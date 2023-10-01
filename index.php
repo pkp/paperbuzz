275c6e41191790fa73f2a659758242e0a93210d8
Bozana Bokan <bozana.bokan@posteo.net>
2023-10-01 18:03:06 +0200

    adapt copyright year and next version number

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 /**
  * @file plugins/generic/paperbuzz/index.php
  *
- * Copyright (c) 2013-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2013-2023 Simon Fraser University
+ * Copyright (c) 2003-2023 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_paperbuzz
